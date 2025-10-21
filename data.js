590113094fdbd15bd561ece9fa8f3f837de37aef
ambteheranesteri <ambteheran.esteri.it@tuta.io>
2025-10-21 12:52:00 +0330

    Create data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+// data.js: اطلاعات کاربران و لینک PDF
+const userData = [
+  {
+    username: "ABDUL_GHAFOOR",
+    password: "2025/659349731",
+    ceuNumber: "659349731",
+    name: "ABDUL GHAFOOR HAKIMI",
+    downloadCode: "1212",
+    links: { finalResult: "docs/FinalDocument.pdf" } // مسیر PDF برای نمایش و دانلود
+  },
+  {
+    username: "user2",
+    password: "pass2",
+    ceuNumber: "67890",
+    name: "SaraM",
+    downloadCode: "1111",
+    links: { finalResult: "docs/AnotherDocument.pdf" }
+  }
+];
